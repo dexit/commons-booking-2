fd06feb9c1b2e218cadcfcf061a41a4a148c67ad
anewholm <annesley_newholm@yahoo.it>
2018-10-27 12:23:38 +0200

    database reflection part I

--- framework/CB2_Query/CB2_Query.php
+++ framework/CB2_Query/CB2_Query.php
@@ -13,7 +13,7 @@ if ( ! function_exists( 'xdebug_print_function_stack' ) ) {
 		if ( WP_DEBUG ) var_dump( debug_backtrace() );
 	}
 }
-define( 'CB2_DEBUG_SAVE', WP_DEBUG && ! defined( 'DOING_AJAX' ) && TRUE );
+define( 'CB2_DEBUG_SAVE', WP_DEBUG && ! defined( 'DOING_AJAX' ) && FALSE );
 
 // Native post create process
 define( 'CB2_CREATE_NEW', -1 );
